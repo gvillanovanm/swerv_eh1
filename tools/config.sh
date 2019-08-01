79c4afcbb775273b2aa8de8a1590b64ae91b9233
Gabriel Villanova <gabriel.magalhaes@embedded.ufcg.edu.br>
2019-08-01 09:37:21 -0300

    Remove Verilator

--- tools/config.sh
+++ tools/config.sh
@@ -1,2 +1,2 @@
 export RV_ROOT=/media/gvillanova/DATA/Github/hw/socs/wd/swerv_eh1
-export VERILATOR_ROOT=/media/gvillanova/DATA/Github/hw/socs/wd/swerv_eh1/tools/verilator-4.010
+#export VERILATOR_ROOT=/media/gvillanova/DATA/Github/hw/socs/wd/swerv_eh1/tools/verilator-4.010
